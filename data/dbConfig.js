bc873f5dd1a77bacecc399fa74e99dccb641419d
Clark <rclark010@gmail.com>
2019-04-15 18:00:00 -0600

    Removes test functions

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
+const userModel = require('../users/users-model').User;
+const fakeUserOne = require('../users/users-model').fakeUserOne;
+
 // The URI is like a URL, but for the remote database
 const uri = process.env.MONGODB_URI;
 // // Create pending connection to remote DB
-mongoose.connect(uri);
+mongoose.connect(uri, { useNewUrlParser: true });
 
 // Access the DB via the connection
 const db = mongoose.connection;
 
 // Log in case there's an error while connecting
 db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function callback() {});
 
 module.exports = db;
