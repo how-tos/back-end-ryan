a13e9a3dd7b1089296db1abb89b5f88aec26c7f3
Clark <rclark010@gmail.com>
2019-04-17 16:50:55 -0600

    Adds mongo connection initializer

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 
+// start Mongo connection
+require('../data/dbConfig');
+
 const authRoutes = require('./auth/auth-routes');
+const howToRoutes = require('./how-to/how-to-routes');
 const usersRoutes = require('./users/users-routes');
 const restrictedMiddleware = require('./auth/restricted-middleware');
 
@@ -14,6 +18,11 @@ server.use(helmet());
 
 // Routing
 server.use('/api/auth', authRoutes);
+server.use('/api/how-to', howToRoutes);
 server.use('/api/users', restrictedMiddleware, usersRoutes);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello!' });
+});
 
 module.exports = server;
